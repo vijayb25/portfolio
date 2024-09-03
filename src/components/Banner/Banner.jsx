212662bb5e2d93f5e3834347f5e49300fadc63cd
Vijay Baclay <vijay.baclay@proweaver.net>
2024-09-03 13:49:50 +0800

    updated resume

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,7 +12,7 @@ function Banner() {
                     <span className="text-md w-max typewriter">Web Developer</span>
                     </div>
                     <p>An aspiring web developer with a passion for creating dynamic and responsive web applications. Proficient in modern web technologies and committed to continuous learning.</p>
-                    <a className="block p-3 bg-red-700 rounded-md hover:bg-red-600" href="/BaclayVijay.pdf" download="BaclayVijay.pdf">Resume</a>
+                    <a className="block p-3 bg-red-700 rounded-md hover:bg-red-600" href="/BaclayVijay.pdf" download="Baclay-Vijay.pdf">Resume</a>
                     
                 </div>
                     <SocialLinks/>
