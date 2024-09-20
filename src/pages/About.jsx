1e5297256228f45881ad45aa5ad159efabf7a49e
Vijay Baclay <vijay.baclay@proweaver.net>
2024-09-20 16:57:57 +0800

    updated resume link in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,7 @@ function About() {
                         <h1 className="block font-bold text-4xl">ABOUT ME</h1>
                     </div>
                     <p className="mt-5 mb-5">Hi! I'm <span className="font-bold text-xl">Vijay</span>, an aspiring web developer with 2 years hands on experience in web development focusing in wordpress. I am 27 years old and currently looking for opportunities that would help me achieve my career goals. I spend most of my time exploring modern web technologies.</p>
-                    <a className="block w-max p-3 bg-red-700 mb-5 rounded-md hover:bg-red-600" href="/BaclayVijay.pdf" download="BaclayVijay.pdf">Resume</a>
+                    <a className="block w-max p-3 bg-red-700 mb-5 rounded-md hover:bg-red-600" href="/Baclay_Vijay.pdf" download="Baclay_Vijay.pdf">Resume</a>
 
                     <h2 className="font-bold text-4xl">Skills</h2>
                     <ul className="flex items-center gap-3 mt-4 flex-wrap">
