ea437c46e66cd11f344df00db0be3364e8e54292
Vijay Baclay <vijay.baclay@proweaver.net>
2024-09-20 16:46:25 +0800

    added form on contact page

---
+++ src/components/Socials/EmailForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+const EmailForm = () => {
+  const [formData, setFormData] = useState({
+    to_name: 'Vijay',
+    subject: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const publicKey = 't8_GwcJtez5humLNF';
+    const serviceID = 'service_aak1s5c';
+    const templateID = 'template_dle2dso';
+
+    emailjs.send(serviceID, templateID, formData, publicKey)
+      .then((response) => {
+        alert('Email sent successfully!');
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((error) => {
+        alert('Failed to send the email.');
+        console.log('FAILED...', error);
+      });
+
+    // Reset form fields after submission
+    setFormData({to_name: 'Vijay', subject: '', email: '', message: '' });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 lg:w-[49%] md:w-[100%] sm:w-full">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="subject">Subject:</label>
+        <input type="text" id="subject" className="text-black w-full p-2 rounded-md" name="subject" value={formData.subject} onChange={handleChange} placeholder="Enter subject..." required />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Email:</label>
+        <input type="email" className="text-black w-full p-2 rounded-md" name="email" value={formData.email} onChange={handleChange} placeholder="Enter your email..." required />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" className="text-black w-full p-2 resize-none rounded-md" name="message" value={formData.message} onChange={handleChange} placeholder="Enter message..." required />
+      </div>
+      <button className="p-4 w-[150px] bg-red-500 rounded-md hover:bg-red-600" type="submit">Send Email</button>
+    </form>
+  );
+};
+
+export default EmailForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import EmailForm from '../components/Socials/EmailForm';
 
 function Contact() {
     return (
         <div className='wrapper'>
-            <div className="mt-40 px-4">
-                <div className="typewriter-container w-max">
-                    <h2 className="font-bold text-4xl uppercase mb-4">Contacts</h2>
+            <div className="lg:flex gap-4 flex-wrap items-center mt-40 px-4 md:flex-row sm:flex-col">
+                <div className="lg:w-[49%] md:w-[100%] sm:w-full">
+                    <h2 className="font-bold text-4xl uppercase mb-4">Get In Touch</h2>
+                    <p>Thank you for visiting my portfolio! If you'd like to discuss a project, collaborate, or just connect, feel free to reach out.</p>
                 </div>
-                <p>You can reach me through these platforms. Looking forward to connecting!</p>
-                <div className="flex gap-2 mt-5">
-                    <a href="mailto:jaybaclay025@gmail.com" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800" >
-                    <i className="fab fa-google fa-2x text-white hover:text-red-600"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/vijay-baclay-3a4837269/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800" >
-                    <i className="fab fa-linkedin fa-2x text-white hover:text-red-600"></i>
-                    </a>
-                </div>
+                <EmailForm/>
+            </div>
+            <div className="flex flex-col gap-2 mt-5 px-4">
+                <h2>You can also contact me through these platforms.</h2>
+                <ul className="flex gap-4">
+                <li><a href="mailto:jaybaclay025@gmail.com" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800" >
+                <i className="fab fa-google fa-2x text-white hover:text-red-600"></i>
+                </a></li>
+                <li><a href="https://www.linkedin.com/in/vijay-baclay-3a4837269/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800" >
+                <i className="fab fa-linkedin fa-2x text-white hover:text-red-600"></i>
+                </a></li>
+                </ul>
             </div>
         </div>
     );
