ba3ebb43616fde5a0e22b834674ba3cc19293b38
Vijay Baclay <vijay.baclay@proweaver.net>
2024-10-04 13:02:23 +0800

    updated resume download link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Resume() {
   return (
     <div>
-        <a className="block w-max p-3 bg-red-700 mb-5 rounded-md hover:bg-red-600" href="/BaclayVijay.pdf" download="BaclayVijay.pdf">Resume</a>
+        <a className="block w-max p-3 bg-red-700 mb-5 rounded-md hover:bg-red-600" href="/VijayBaclay.pdf" download="VijayBaclay.pdf">Resume</a>
     </div>
   )
 }
